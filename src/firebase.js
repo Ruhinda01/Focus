4d29b18650aebf5a1e7b883b16f5a7504dc3d98d
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2024-09-10 11:03:03 +0300

    Fixed authentication issues

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAMoetg02tqRWSxj_Nfj8uL9kvqoP9ET64",
-  authDomain: "focus-b63d7.firebaseapp.com",
-  projectId: "focus-b63d7",
-  storageBucket: "focus-b63d7.appspot.com",
-  messagingSenderId: "374539647504",
-  appId: "1:374539647504:web:1cd68c486e1c673d2fa895",
-  measurementId: "G-XW0CZTL8PN"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
